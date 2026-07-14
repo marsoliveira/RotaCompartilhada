4ba33b33bd658a6b32a52ffc8d71b3cfc1817c2d
Marcela <marcela.sdeo4@gmail.com>
2026-07-14 17:08:32 -0300

    adiciona classe Passageiro

---
+++ src/main/java/br/ufjf/dcc/model/Passageiro.java
@@ -0,0 +1,8 @@
+package br.ufjf.dcc.model;
+
+public class Passageiro extends Pessoa {
+
+    public Passageiro(String nome, String cpf, Endereco endereco) {
+        super(nome, cpf, endereco);
+    }
+}
