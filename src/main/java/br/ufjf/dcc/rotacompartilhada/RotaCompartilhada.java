54a8ef1b27f7c6f0b1bf60019b4e34cc4b915804
Marcela <marcela.sdeo4@gmail.com>
2026-07-14 16:28:20 -0300

    cria projeto

---
+++ src/main/java/br/ufjf/dcc/rotacompartilhada/RotaCompartilhada.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.ufjf.dcc.rotacompartilhada;
+
+/**
+ *
+ * @author marce
+ */
+public class RotaCompartilhada {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
