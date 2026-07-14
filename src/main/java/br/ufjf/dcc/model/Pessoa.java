e3d08cc9e26c9c208d31e83998edf7bea06c1a26
Marcela <marcela.sdeo4@gmail.com>
2026-07-14 17:08:17 -0300

    adiciona classe abstrata Pessoa

---
+++ src/main/java/br/ufjf/dcc/model/Pessoa.java
@@ -0,0 +1,38 @@
+package br.ufjf.dcc.model;
+
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private Endereco endereco;
+
+	public Pessoa(String nome, String cpf, Endereco endereco) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+}
