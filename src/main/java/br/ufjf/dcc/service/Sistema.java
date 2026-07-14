2558d345a485f5f5ea568c96baf059d1b81f3efb
Marcela <marcela.sdeo4@gmail.com>
2026-07-14 17:54:23 -0300

    adiciona classe Sistema com métodos para gerenciamento de motoristas, passageiros e caronas

---
+++ src/main/java/br/ufjf/dcc/service/Sistema.java
@@ -0,0 +1,87 @@
+package br.ufjf.dcc.service;
+
+import java.util.List;
+
+import br.ufjf.dcc.model.Carona;
+import br.ufjf.dcc.model.Motorista;
+import br.ufjf.dcc.model.Passageiro;
+
+public class Sistema {
+
+    private List<Motorista> motoristas;
+    private List<Passageiro> passageiros;
+    private List<Carona> agendadas;
+    private List<Carona> emAndamento;
+    private List<Carona> finalizadas;
+
+    public void cadastrarMotorista() {
+
+    }
+
+    public void exibirMotorista() {
+
+    }
+
+    public void editarMotorista() {
+
+    }
+
+    public void removerMotorista() {
+
+    }
+
+    public void listarMotoristas() {
+
+    }
+
+    public void cadastrarPassageiro() {
+
+    }
+
+    public void exibirPassageiro() {
+
+    }
+
+    public void editarPassageiro() {
+
+    }
+
+    public void removerPassageiro() {
+
+    }
+
+    public void listarPassageiros() {
+
+    }
+
+    public void cadastrarCarona() {
+
+    }
+
+    public void agendarCarona() {
+
+    }
+
+    // public boolean verificarStatusCarona() {
+        
+    // }
+
+    public void finalizarCarona() {
+    }
+
+    public void listarCaronasAgendadas() {
+
+    }
+
+	public void listarCaronasEmAndamento() {
+
+    }
+
+	public void listarCaronasFinalizadas() {
+
+    }
+
+    public void atualizarSistema() {
+
+    }
+}
