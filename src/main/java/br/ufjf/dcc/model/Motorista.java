84f1c41c0543f57e73c1dd6fd2daf4d262dc6709
Marcela <marcela.sdeo4@gmail.com>
2026-07-14 17:53:18 -0300

    refatora construtor de Motorista

--- src/main/java/br/ufjf/dcc/model/Motorista.java
+++ src/main/java/br/ufjf/dcc/model/Motorista.java
@@ -5,10 +5,10 @@ public class Motorista extends Pessoa {
     private Veiculo veiculo;
     private boolean disponivel;
 
-    public Motorista(String nome, String cpf, Endereco endereco, Veiculo veiculo) {
+	public Motorista(String nome, String cpf, Endereco endereco, Veiculo veiculo, boolean disponivel) {
         super(nome, cpf, endereco);
         this.veiculo = veiculo;
-        this.disponivel = true;
+        this.disponivel = disponivel;
     }
 
     public void setVeiculo(Veiculo veiculo) {
