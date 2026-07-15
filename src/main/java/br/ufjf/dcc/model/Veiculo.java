d2b9f48dde858500b67f9adf552ee775382b40f9
Marcela <marcela.sdeo4@gmail.com>
2026-07-14 21:12:32 -0300

    remove validação de ano de fabricação em Veiculo

--- src/main/java/br/ufjf/dcc/model/Veiculo.java
+++ src/main/java/br/ufjf/dcc/model/Veiculo.java
@@ -9,78 +9,70 @@ public class Veiculo {
     private int anoFabricacao;
     private String cor;
 
-	private static final int LIM_ANO_FABRICACAO = 2016;
+    public Veiculo(String nome, String modelo, String placa, String chassi, int anoFabricacao, String cor) {
+        this.nome = nome;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.chassi = chassi;
+        this.anoFabricacao = anoFabricacao;
+        this.cor = cor;
+    }
 
-	public Veiculo(String nome, String modelo, String placa, String chassi, int anoFabricacao, String cor) {
-		this.nome = nome;
-		this.modelo = modelo;
-		this.placa = placa;
-		this.chassi = chassi;
-		if (validarAnoFabricacao(anoFabricacao)) {
-			this.anoFabricacao = anoFabricacao;
-		}
-		this.cor = cor;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
+    public String getModelo() {
+        return modelo;
+    }
 
-	public String getModelo() {
-		return modelo;
-	}
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
+    public String getPlaca() {
+        return placa;
+    }
 
-	public String getPlaca() {
-		return placa;
-	}
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
 
-	public void setChassi(String chassi) {
-		this.chassi = chassi;
-	}
+    public String getChassi() {
+        return chassi;
+    }
 
-	public String getChassi() {
-		return chassi;
-	}
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
 
-	public void setAnoFabricacao(int anoFabricacao) {
-		this.anoFabricacao = anoFabricacao;
-	}
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
 
-	public int getAnoFabricacao() {
-		return anoFabricacao;
-	}
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 
-	public void setCor(String cor) {
-		this.cor = cor;
-	}
+    public String getCor() {
+        return cor;
+    }
 
-	public String getCor() {
-		return cor;
-	}
-
-	public boolean validarAnoFabricacao(int anoFabricacao) {
-		return anoFabricacao >= LIM_ANO_FABRICACAO;
-	}
-
-	public void exibirDados() {
-		System.out.println("Dados do veículo:");
-		System.out.println("Nome: " + this.nome);
-		System.out.println("Modelo: " + this.modelo);
-		System.out.println("Placa: " + this.placa);
-		System.out.println("Chassi: " + this.chassi);
-		System.out.println("Ano de Fabricação: " + this.anoFabricacao);
-		System.out.println("Cor: " + this.cor);
-	}
+    public void exibirDados() {
+        System.out.println("Dados do veículo:");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Placa: " + this.placa);
+        System.out.println("Chassi: " + this.chassi);
+        System.out.println("Ano de Fabricação: " + this.anoFabricacao);
+        System.out.println("Cor: " + this.cor);
+    }
 }
